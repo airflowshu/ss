a269ea312f26d3c0f3c0fd592ede6d8cab56f892
airflowshu <46051027+airflowshu@users.noreply.github.com>
2024-08-21 19:09:25 +0800

    Update r3_ss.sh

--- r3_ss.sh
+++ r3_ss.sh
@@ -12,7 +12,7 @@ if [[ $EUID -ne 0 ]]; then
 fi
 cd /tmp
 rm -f shadowsocks_r3.tar.gz
-curl https://github.com/blademainer/miwifi-ss/blob/master/r3/shadowsocks_r3.tar.gz
+curl https://github.com/airflowshu/ss/blob/main/shadowsocks_r3.tar.gz
 tar zxf shadowsocks_r3.tar.gz
 
 # install shadowsocks ss-redir to /data/usr/sbin
